3aafe38ce070114318f346f7dea80a6555c9af8c
Jesus Melendez <jmelendez@technisys.com>
2019-07-03 11:21:55 -0400

    commit inicial bases node

---
+++ app.js
@@ -0,0 +1,19 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`archivo creado: `, colors.green(archivo)))
+            .catch(err => console.log(err));
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
